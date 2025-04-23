77eae2080d0aa29fe25717f278c2be8d4dbbd283
customcapbd <dtfinkbd@gmail.com>
2025-04-23 11:42:26 +0600

    Add files via upload

---
+++ product-data.js
@@ -0,0 +1,26 @@
+window.productData = {
+  p1: {
+    name: "Classic Cap Red",
+    price: 550,
+    images: [
+      "https://cdn.mockupnest.com/wp-content/uploads/edd/2024/10/19-Free-Baseball-Cap-Mockup.jpg",
+      "https://mockups-design.com/wp-content/uploads/2021/08/Free_Baseball_Cap_Mockup_3.jpg"
+    ]
+  },
+  p2: {
+    name: "Sporty Cap Blue",
+    price: 480,
+    images: [
+      "https://mockups-design.com/wp-content/uploads/2021/08/Free_Baseball_Cap_Mockup_3.jpg",
+      "https://cdn.mockupnest.com/wp-content/uploads/edd/2024/10/19-Free-Baseball-Cap-Mockup.jpg"
+    ]
+  },
+  p3: {
+    name: "Urban Street Hat",
+    price: 620,
+    images: [
+      "https://mockups-design.com/wp-content/uploads/2021/08/Free_Cap_Mockup_2.jpg",
+      "https://cdn.mockupnest.com/wp-content/uploads/edd/2023/11/urban-hat.jpg"
+    ]
+  }
+};
